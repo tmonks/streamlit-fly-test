fc2465d8ea01bff7ea683603f68b4e591d8c46cc
Tom Monks <monksy@gmail.com>
2022-11-21 10:14:32 -0500

    Use environment variables for passwords

--- auth.py
+++ auth.py
@@ -1,20 +1,32 @@
 import streamlit as st
 import streamlit_authenticator as stauth
-import yaml
+import os
 
-with open('config.yml') as file:
-    config = yaml.load(file, Loader=yaml.SafeLoader)
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+USER_PASSWORD = os.environ.get('USER_PASSWORD')
+SIGNATURE_KEY = os.environ.get('SIGNATURE_KEY')
 
 
 def ensure_authenticated():
 
+    usernames = {
+        'usernames':
+            {
+                'admin': {
+                    'email': 'admin@example',
+                    'name': 'admin',
+                    'password': ADMIN_PASSWORD
+                },
+                'user': {
+                    'email': 'user@example.com',
+                    'name': 'user',
+                    'password': USER_PASSWORD
+                }
+            }
+    }
+
     authenticator = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days'],
-        config['preauthorized']
-    )
+        usernames, 'my_app_cookie_name', SIGNATURE_KEY, 30)
 
     if 'authentication_status' not in st.session_state or not st.session_state['authentication_status']:
         authenticator.login('Log in', 'main')
